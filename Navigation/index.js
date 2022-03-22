c2e98612cbc7bf2b5b94e5dedf7b30022e3a9dab
Ahmad <ahmad_baq@hotmail.com>
2022-03-22 19:15:21 +0300

    add simple modal for add queue

--- Navigation/index.js
+++ Navigation/index.js
@@ -4,18 +4,20 @@ import React from "react";
 import Test1 from "../components/tests/Test1";
 import Test2 from "../components/tests/Test2";
 import { observer } from "mobx-react";
-import Signin from "../Auth/Signin";
-import Signup from "../Auth/Signup";
+import Signin from "../components/Auth/Signin";
+import Signup from "../components/Auth/Signup";
 import Home from "../components/tests/Home";
 import QueueList from "../components/queues/QueueList";
 import MemberList from "../components/members/MemberList";
-
+import authStore from "../stores/authStore";
 
 const { Navigator, Screen } = createStackNavigator();
 
 const RootNavigator = () => {
+  //const user = authStore.user;
+  const user = "hi";
   return (
-    <Navigator initialRouteName="Home">
+    <Navigator initialRouteName={user ? "QueueList" : "Home"}>
       <Screen
         name="Home"
         component={Home}
@@ -37,7 +39,6 @@ const RootNavigator = () => {
           headerShown: false,
         }}
       ></Screen>
-    <Navigator initialRouteName="QueueList">
 
       <Screen name="TestA" component={Test1} />
       <Screen name="TestB" component={Test2} />
