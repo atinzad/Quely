b9ea9cd28d01fc436b7d8f8fb2973ca4d93730e5
Ahmad <ahmad_baq@hotmail.com>
2022-03-23 15:55:58 +0300

    add memeber integrated and fixed queue to queues for route

---
+++ components/members/MemberItem.js
@@ -0,0 +1,64 @@
+import {
+  Dimensions,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React from "react";
+import { HStack, VStack } from "native-base";
+
+const MemberItem = ({ member, navigation }) => {
+  //
+  return (
+    <Pressable
+      style={styles.button}
+      onPress={() => console.log("this is a memeber")}
+    >
+      <VStack style={styles.container}>
+        <Text style={styles.title}>{member.email}</Text>
+      </VStack>
+    </Pressable>
+  );
+};
+
+export default MemberItem;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    width: Dimensions.get("window").width,
+    paddingBottom: 40,
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginBottom: 20,
+    marginTop: 20,
+    borderBottomColor: "black",
+    borderBottomWidth: 2,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    position: "relative",
+    borderRadius: 20,
+  },
+  shadow: {
+    position: "absolute",
+    width: "100%",
+    height: 200,
+    backgroundColor: "rgba(0, 0, 0, 0.450)",
+    bottom: 10,
+    zIndex: 1,
+    borderRadius: 20,
+  },
+  title: {
+    position: "absolute",
+    color: "black",
+    fontSize: 25,
+    zIndex: 2,
+    fontWeight: "bold",
+  },
+});
