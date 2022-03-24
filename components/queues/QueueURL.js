300a28656e67dd61c0452983359c3b24af618764
Ahmad <ahmad_baq@hotmail.com>
2022-03-24 19:02:23 +0300

    fixed hyperlink to port 3000, add baseFormURL variable

--- components/queues/QueueURL.js
+++ components/queues/QueueURL.js
@@ -1,18 +1,18 @@
 import { Alert, Clipboard, StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { baseURL } from "../../stores/instance";
+import { baseFormURL } from "../../stores/instance";
 import { Button } from "native-base";
 import QRCode from "react-native-qrcode-svg";
 
 const QueueURL = ({ queue }) => {
   const copyToClipboard = () => {
-    Clipboard.setString(`${baseURL}form/${queue._id}`);
+    Clipboard.setString(`${baseFormURL}form/${queue._id}`);
     Alert.alert("copied to clipboard");
   };
 
   return (
     <View>
-      <QRCode value={`${baseURL}form/${queue._id}`} />
+      <QRCode value={`${baseFormURL}form/${queue._id}`} />
       <Button onPress={copyToClipboard}>copy URL</Button>
     </View>
   );
